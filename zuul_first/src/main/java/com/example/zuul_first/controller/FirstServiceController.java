222f1891c66ed0145260b61ed9276b91f54dc31f
YuSunjo <tnswh2023@gmail.com>
2021-05-12 17:35:16 +0900

    filter, custom filter 적용

--- zuul_first/src/main/java/com/example/zuul_first/controller/FirstServiceController.java
+++ zuul_first/src/main/java/com/example/zuul_first/controller/FirstServiceController.java
@@ -1,16 +1,33 @@
 package com.example.zuul_first.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// http://localhost:8081/welcome - zuul Service에서는 이렇게 받음
+// http://localhost:8081/first-service/welcome - gateway에서는 이렇게 받음
 @RestController
-@RequestMapping("/")
+@RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "welcome to fisrt service";
+        return "welcome to first service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader(value = "first-request") String header) {
+        log.info(header);
+        return "hello";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "hi first service";
     }
 
 }
--- zuul_second_service/src/main/java/com/example/zuul_second_service/controller/SecondServiceController.java
+++ zuul_second_service/src/main/java/com/example/zuul_second_service/controller/SecondServiceController.java
@@ -1,16 +1,30 @@
 package com.example.zuul_second_service.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
+@Slf4j
 public class SecondServiceController {
 
     @GetMapping("/welcome")
     public String second() {
         return "welcome second service";
-
     }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "message";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "hi second service";
+    }
+
 }
